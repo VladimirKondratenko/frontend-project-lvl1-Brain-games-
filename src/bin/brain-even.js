cf5a87eb0f61a1991dcf44dd9fd75e2241dd8098
Vladimir <v.n.kondratenko@yandex.by>
2020-02-19 22:43:28 +0300

    step 4

---
+++ src/bin/brain-even.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import getUserName from '..';
+
+const checkEvenNumber = () => {
+  const userName = getUserName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * Math.floor(1000));
+    const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (rightAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+checkEvenNumber();
