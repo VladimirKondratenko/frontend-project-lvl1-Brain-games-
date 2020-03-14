4da48b8b1fe9c44d398340015800b48783056c89
Vladimir <v.n.kondratenko@yandex.by>
2020-03-14 22:26:35 +0300

    add brain-calc

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import getUserName from '..';
+
+const calculatorGame = () => {
+  const userName = getUserName();
+  console.log('What is the result of the expression?');
+  for (let i = 0; i < 3; i += 1) {
+    const symbolsArr = ['+', '-', '*'];
+    const randomNumberOne = Math.floor(Math.random() * Math.floor(100));
+    const randomNumberTwo = Math.floor(Math.random() * Math.floor(100));
+    const symbolIndex = Math.floor(Math.random() * Math.floor(3));
+    let rightAnswer;
+    switch (symbolsArr[symbolIndex]) {
+      case '+':
+        rightAnswer = randomNumberOne + randomNumberTwo;
+        break;
+      case '-':
+        rightAnswer = randomNumberOne - randomNumberTwo;
+        break;
+      case '*':
+        rightAnswer = randomNumberOne * randomNumberTwo;
+        break;
+      default:
+        break;
+    }
+    console.log(`Question: ${randomNumberOne} ${symbolsArr[symbolIndex]} ${randomNumberTwo}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (rightAnswer === parseInt(answer, 10)) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+calculatorGame();
