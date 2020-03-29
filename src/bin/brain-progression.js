1f7c462926cb28af6bf846eaa568319ffaed5435
Vladimir <v.n.kondratenko@yandex.by>
2020-03-29 15:12:31 +0300

    brain-progression generate

--- src/bin/brain-progression.js
+++ src/bin/brain-progression.js
@@ -2,21 +2,23 @@
 import readlineSync from 'readline-sync';
 import getUserName from '..';
 
-const greatestCommonDivisorGame = () => {
+const getArithmeticProgression = () => {
   const userName = getUserName();
   console.log('What number is missing in the progression?');
   for (let i = 0; i < 3; i += 1) {
-    const randomNumberOne = Math.floor(Math.random() * Math.floor(100));
-    const randomNumberTwo = Math.floor(Math.random() * Math.floor(10));
-    let rightAnswer;
-    console.log(`Question: ${randomNumberOne} ${randomNumberTwo}`);
-    for (let count = randomNumberOne; count > 0; count -= 1) {
-      if (randomNumberOne % count === 0 && randomNumberTwo % count === 0) {
-        rightAnswer = count;
-      } else {
-        rightAnswer = 0;
-      }
+    let startNumberProgression = Math.floor(Math.random() * Math.floor(100));
+    const numberChangeProgression = Math.floor(Math.random() * Math.floor(100));
+    const resultArithmeticProgression = [];
+    const rightAnswer = numberChangeProgression;
+
+    for (let count = 1; count < 10; count += 1) {
+      startNumberProgression += numberChangeProgression;
+      resultArithmeticProgression.push(startNumberProgression);
+      // console.log(`${startNumberProgression} start progression`);
+      // console.log(`${numberChangeProgression} numberchangeprogression`);
+      // console.log(resultArithmeticProgression + 'TEST');
     }
+    console.log(`Question: ${resultArithmeticProgression}`);
     const answer = readlineSync.question('Your answer: ');
     if (rightAnswer === parseInt(answer, 10)) {
       console.log('Correct!');
@@ -29,4 +31,4 @@ const greatestCommonDivisorGame = () => {
   console.log(`Congratulations, ${userName}!`);
 };
 
-greatestCommonDivisorGame();
+getArithmeticProgression();
