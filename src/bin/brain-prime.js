834d27cc16e1a36abcbaf1c675d05c5f0040f78e
Vladimir <v.n.kondratenko@yandex.by>
2020-03-29 15:55:05 +0300

    add ascinema

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import getUserName from '..';
+
+const getArithmeticProgression = () => {
+  const userName = getUserName();
+  console.log('What number is missing in the progression?');
+  for (let i = 0; i < 3; i += 1) {
+    let startNumberProgression = Math.floor(Math.random() * Math.floor(100));
+    const numberChangeProgression = Math.floor(Math.random() * Math.floor(100));
+    const indexProgression = Math.floor(Math.random() * Math.floor(10));
+    const resultArithmeticProgression = [];
+    let rightAnswer = 0;
+
+    for (let count = 1; count < 10; count += 1) {
+      startNumberProgression += numberChangeProgression;
+      resultArithmeticProgression.push(startNumberProgression);
+    }
+
+    rightAnswer = resultArithmeticProgression[indexProgression];
+    resultArithmeticProgression[indexProgression] = '..';
+
+    console.log(`Question: ${resultArithmeticProgression.join(' ')}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (rightAnswer === parseInt(answer, 10)) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+getArithmeticProgression();
